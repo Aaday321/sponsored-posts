1adcc459f3ebdcd9c8d83081e81cf6cb50494406
aaday321 <aderitchards@gmail.com>
2025-11-27 13:13:48 -0800

    project set up

---
+++ app/(tabs)/chat/index.tsx
@@ -0,0 +1,33 @@
+import {View, Text, StyleSheet, ScrollView, Button, Pressable} from "react-native";
+
+export default function Chat() {
+
+    function handlePress() {
+        console.log("Press");
+    }
+
+    return (
+        <View style={styles.bg}>
+            <Text style={styles.text}>Chat</Text>
+            <Pressable style={styles.btn} onPress={handlePress}>
+                <Text >Press Me</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    bg: {
+        backgroundColor: "black",
+        height: "100%",
+    },
+    text: {
+        marginTop: 50,
+        color: "white",
+    },
+    btn: {
+        backgroundColor: "white",
+        padding: 20,
+    }
+});
