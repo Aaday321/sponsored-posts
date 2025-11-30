35a0d2717f726b730c9ce21746b6e285aa9582e1
Ade <aderitchards@gmail.com>
2025-11-30 05:52:52 -0800

    small adjustment

--- app/components/PriceBubble.tsx
+++ app/components/PriceBubble.tsx
@@ -18,8 +18,8 @@ export default function PriceBubble({ price, timestamp, isFromUser }: PriceBubbl
                         styles.priceTimestamp,
                         {
                             textAlign: isFromUser ? 'right' : 'left',
-                            paddingLeft: !isFromUser ? META_TEXT_PADDING : 0,
-                            paddingRight: !isFromUser ? 0 : META_TEXT_PADDING,
+                            paddingRight: isFromUser ? META_TEXT_PADDING : 0,
+                            paddingLeft: isFromUser ? 0 : META_TEXT_PADDING,
                         }
                     ]}
                 >
@@ -46,6 +46,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#00CB4E",
         borderRadius: 16,
         paddingHorizontal: 18,
+        paddingTop: 2
     },
     priceTimestamp: {
         fontSize: 12,
@@ -56,7 +57,7 @@ const styles = StyleSheet.create({
     priceText: {
         fontSize: 42,
         fontFamily: 'Koulen',
-        color: '#000',
+        color: '#045623',
     },
 })
 
